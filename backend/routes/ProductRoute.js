c5804acb7e728b5a6a0ef92192ece3e0eea7d7fa
unnnt <d400200062@student.ums.ac.id>
2024-05-02 19:41:19 +0700

    barang masuk

--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -3,6 +3,7 @@ import {
     getProducts,
     getProductById,
     createProduct,
+    createProductWithbarangMasuk,
     updateProduct,
     deleteProduct
 } from "../controllers/ProductController.js"
@@ -11,6 +12,7 @@ const router = express.Router()
 router.get('/products', getProducts)
 router.get('/products/:id', getProductById)
 router.post('/products', createProduct)
+router.post('/products/create-with-masuk', createProductWithbarangMasuk);
 router.patch('/products/:id', updateProduct)
 router.delete('/products/:id', deleteProduct)
 
