40690cc190af951b5ff425b8dbffbd3397f97d79
Ngurah Widi <widi050917@gmail.com>
2024-05-07 21:24:58 +0800

    membuat product list dan crud

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Login from './components/Login';
 import ProductList from './components/ProductList';
+import AddProduct from './components/AddProduct';
+import EditProduct from './components/EditProduct';
 
 const App = () => {
   return (
    <BrowserRouter>
     <Routes>
-      <Route path="/Login" element={<Login/>}/>
-      <Route path="/Products" element={<ProductList/>}/>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/products" element={<ProductList/>}/>
+      <Route path="/add" element={<AddProduct/>}/>
+      <Route path="/edit/:id" element={<EditProduct/>}/>
     </Routes>
    </BrowserRouter>
   );
---
+++ frontend/src/components/EditProduct.jsx
@@ -0,0 +1,97 @@
+import {React, useState, useEffect} from 'react'
+import axios from 'axios'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const EditProduct = () => {
+  const [kode, setKode] = useState("")
+  const [nama, setNama] = useState("")
+  const [harga, setHarga] = useState("")
+  const [stok, setStok] = useState("")
+  const [deskripsi, setDeskripsi] = useState("")
+  const navigate = useNavigate()
+  const { id } = useParams()
+
+  useEffect(()=> {
+    const getProductById = async ()=> {
+        const response = await axios.get(`http://localhost:5000/products/${id}`)
+        setKode(response.data.kode)
+        setNama(response.data.nama)
+        setHarga(response.data.harga)
+        setStok(response.data.stok)
+        setDeskripsi(response.data.deskripsi)
+    }
+    getProductById()
+  }, [id])
+
+  const updateProduct = async (e)=>{
+    e.preventDefault()
+    await axios.patch(`http://localhost:5000/products/${id}`, {
+      kode: kode,
+      nama: nama,
+      harga: parseInt(harga),
+      stok: parseInt(stok),
+      deskripsi: deskripsi,
+    })
+    navigate("/products")
+  }
+  return (
+    <div className='max-w-xl mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-500'>
+      <h1 className='text-center font-bold text-lg'>Form Edit Barang</h1>
+      <form onSubmit={updateProduct} className='my-10'>
+        <div className='flex flex-col'>
+          <div className='mb-5'>
+            <label className='font-bold text-slate-700'>Kode</label>
+            <input type="text" 
+            className='w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow'
+            placeholder='Kode barang'
+            value={kode}
+            onChange={(e)=> setKode(e.target.value)}
+            />
+          </div>
+          <div className='mb-5'>
+            <label className='font-bold text-slate-700'>Nama</label>
+            <input type="text" 
+            className='w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow'
+            placeholder='Nama barang'
+            value={nama}
+            onChange={(e)=> setNama(e.target.value)}
+            />
+          </div>
+          <div className='mb-5'>
+            <label className='font-bold text-slate-700'>Harga</label>
+            <input type="text" 
+            className='w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow'
+            placeholder='Harga barang'
+            value={harga}
+            onChange={(e)=> setHarga(e.target.value)}
+            />
+          </div>
+          <div className='mb-5'>
+            <label className='font-bold text-slate-700'>Stok</label>
+            <input type="text" 
+            className='w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow'
+            placeholder='Stok barang'
+            value={stok}
+            onChange={(e)=> setStok(e.target.value)}
+            />
+          </div>
+          <div className='mb-5'>
+            <label className='font-bold text-slate-700'>Deskripsi</label>
+            <input type="text" 
+            className='w-full py-3 mt-1 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow'
+            placeholder='Deskripsi barang'
+            value={deskripsi}
+            onChange={(e)=> setDeskripsi(e.target.value)}
+            />
+          </div>
+          <button 
+          type='submit' 
+          className='w-full py-3 font-bold text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow'>
+          Update</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default EditProduct
