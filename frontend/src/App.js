5af78f4ce0029fafdf86c2a7488ccb81d0c6b266
Bsuhwuu <ridhonovan951@gmail.com>
2024-05-12 18:24:34 +0700

    Dashboard cuy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import Login from './components/Login';
 import ProductList from './components/ProductList';
 import AddProduct from './components/AddProduct';
 import EditProduct from './components/EditProduct';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
   return (
    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/products" element={<ProductList/>}/>
       <Route path="/add" element={<AddProduct/>}/>
       <Route path="/edit/:id" element={<EditProduct/>}/>
